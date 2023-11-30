523e5afe6364db9e7a155b0892b697f88a2f0cad
udy <yudai.kaneko@sbir3japan.co.jp>
2023-11-30 23:47:03 +0900

    Not Result founded エラーまで

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  SocialPostCollection 
+ } from './ui-components';
+ 
 
 function App() {
   return (
     <div className="App">
+            <SocialPostCollection />
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +24,7 @@ function App() {
           Learn React
         </a>
       </header>
+
     </div>
   );
 }
